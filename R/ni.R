efb590a58327db1b14b2c71f4a55a61b7e3f1a1d
smach <smachlis@gmail.com>
2015-03-10 20:58:25 -0400

    Added function %ni%

---
+++ R/ni.R
@@ -0,0 +1,15 @@
+#' %ni%
+#'
+#' Base R has %in% to see if one object is contained within another object.
+#' Why not %ni% to see if one object is NOT contained within another?
+#' Idea from stackoverflow
+#' http://stackoverflow.com/questions/11302985/match-with-negation
+#'
+#' @param myobject (a vector)
+#' @keywords function
+#' @export
+#' @examples
+#' 5 %ni% c(2,4,6)
+#' # TRUE
+#'
+'%ni%' <- Negate('%in%')
