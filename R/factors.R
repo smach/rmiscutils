cea5feea60e239d972959c35cf0bdb9f8999d233
smach <smachlis@gmail.com>
2015-05-10 09:05:43 -0400

    Updated documentation

--- R/factors.R
+++ R/factors.R
@@ -3,6 +3,7 @@
 #' Turn a vector of strings into a factors while keeping the existing order of elements in the vector, instead of having them automatically alphabetized.
 #' @keywords ordered factors
 #' @export
+#' @param myvector vector of character strings
 #' @examples
 #' mymonths <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
 #' ordered_months_as_factors <- strings.to.ordered.factors(mymonths)
