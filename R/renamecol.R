ce42315da5e6d37de5cf3bb3136fd8dd6d8b3a6a
smach <smachlis@gmail.com>
2016-06-02 08:15:03 -0400

    Added renamecol function

---
+++ R/renamecol.R
@@ -0,0 +1,20 @@
+#' renamecol
+#' This function is a wrapper for the more cumbersome base R syntax
+#' colnames(mydf)[colnames(mydf) == 'oldcolname'] <- 'newcolname'
+#' because I also find it difficult to remember that I could also use
+#' mydf <- dplyr::rename(mydf,c('foo'='samples'))
+#'
+#' @param mydf dataframe with column to be renamed
+#' @param oldcolname character string existing column name
+#' @param newcolname character string desired new column name
+#'
+#' @return dataframe
+#' @export
+#'
+#' @examples
+#' new_mtcars <- renamecol(mtcars, "mpg", "MPG")
+#'
+renamecol <- function(mydf, oldcolname, newcolname){
+  colnames(mydf)[colnames(mydf) == oldcolname] <- newcolname
+  return(mydf)
+}
